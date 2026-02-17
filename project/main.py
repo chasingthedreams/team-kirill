6a121b4648bc6e78f18834b57186ec0068db26b9
gtyyjo <i37013851@gmail.com>
2026-02-17 13:48:12 +0300

    main.py

--- project/main.py
+++ project/main.py
@@ -9,3 +9,12 @@ def reset():
     global count
     count = 0
     label.config(text=f"Кликов: {count}")
+window = tk.Tk()
+window.title("Счетчик кликов")
+window.geometry("250x200")
+
+count = 0
+
+# Кнопка для кликов
+tk.Button(window, text="Кликни меня!", command=click, 
+          bg="lightblue", font=("Arial", 14)).pack(pady=20)
