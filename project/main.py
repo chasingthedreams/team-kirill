d2a5232181cbcdf08977d7eec13b715948a6750f
gtyyjo <i37013851@gmail.com>
2026-02-17 13:48:34 +0300

    main.py

--- project/main.py
+++ project/main.py
@@ -15,6 +15,6 @@ window.geometry("250x200")
 
 count = 0
 
-# Кнопка для кликов
+
 tk.Button(window, text="Кликни меня!", command=click, 
           bg="lightblue", font=("Arial", 14)).pack(pady=20)
